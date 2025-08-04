bc5206715cc9fa6c36e62cb7ef56cdedaeaa0170
Dan Jaheny <danjaheny@dans-macbook-pro.home>
2025-08-04 22:44:34 +0200

    spl-transfer-gill-solana

---
+++ typescript/solana/gill/spl-transfer/src/run.ts
@@ -0,0 +1,35 @@
+import { createTx } from './serialize-spl-transfer';
+import { createAndSignTx, get_tx } from './process_tx';
+import { signWithPrivateKey } from './signer';
+import { fordefiConfig, transferConfig } from './config';
+
+
+async function main(): Promise<void> {
+  // Build and sign the transaction payload
+  const requestBody = JSON.stringify(await createTx(fordefiConfig, transferConfig));
+  const timestamp = new Date().getTime();
+  const feePayerVaultPayload = `${fordefiConfig.apiPathEndpoint}|${timestamp}|${requestBody}`;
+  const signedPayloadOne = await signWithPrivateKey(feePayerVaultPayload, fordefiConfig.privateKeyPem);
+
+  try {
+  console.log("Submitting transaction to Fordefi for MPC signature 🔑")
+  const response = await createAndSignTx(
+    fordefiConfig.apiPathEndpoint, 
+    fordefiConfig.accessToken, 
+    signedPayloadOne, 
+    timestamp, 
+    requestBody
+  );
+  if (response){
+      console.log("Transaction fully signed and submitted to network ✅");
+      console.log(`Final transaction ID: ${response.data.id}`);
+      const fullySignedTx = await get_tx(fordefiConfig.apiPathEndpoint, fordefiConfig.accessToken,response.data.id)
+      console.log(fullySignedTx.explorer_url);
+    }
+  } catch (error: any) {
+    console.error(`Failed to sign the transaction: ${error.message}`);
+  }
+}
+if (require.main === module) {
+  main();
+}
---
+++ typescript/solana/gill/spl-transfer/src/serialize-spl-transfer.ts
@@ -0,0 +1,48 @@
+import * as gill from 'gill';
+import { TOKEN_PROGRAM_ADDRESS } from '@solana-program/token';
+import { FordefiSolanaConfig, TransferConfig } from './config';
+import { buildTransferTokensTransaction } from "gill/programs/token";
+
+
+export async function createTx(fordefiConfig: FordefiSolanaConfig, transferConfig: TransferConfig){
+    const rpc = gill.createSolanaRpc(transferConfig.mainnetRpc);
+    const sourceVault = gill.address(fordefiConfig.originAddress);
+    const sourceVaultSigner = gill.createNoopSigner(sourceVault);
+    const destVault = gill.address(fordefiConfig.destAddress);
+    const usdcMint = gill.address(transferConfig.mint);
+
+    const { value: latestBlockhash } = await rpc.getLatestBlockhash().send();
+
+    const transferTokensTx = await buildTransferTokensTransaction({
+      feePayer: sourceVaultSigner,
+      latestBlockhash,
+      mint: usdcMint,
+      authority: sourceVaultSigner,
+      amount: transferConfig.amount,
+      destination: destVault,
+      tokenProgram: TOKEN_PROGRAM_ADDRESS,
+    });
+    console.log("Tx message: ", transferTokensTx);
+    console.log("Tx instructions detailed:", JSON.stringify(transferTokensTx.instructions, null, 2));
+
+    const signedTx = await gill.partiallySignTransactionMessageWithSigners(transferTokensTx);
+    console.log("Signed transaction: ", signedTx);
+
+    const base64EncodedData = Buffer.from(signedTx.messageBytes).toString('base64');
+    console.debug("Raw data ->", base64EncodedData)
+
+    const jsonBody = {
+        vault_id: fordefiConfig.originVault,
+        signer_type: "api_signer",
+        sign_mode: "auto",
+        type: "solana_transaction",
+        details: {
+            type: "solana_serialized_transaction_message",
+            push_mode: "auto",
+            chain: "solana_mainnet",
+            data: base64EncodedData
+        }
+    };
+
+    return jsonBody;
+}
