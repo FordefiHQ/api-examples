ce75bdf004d6c98a4f68c8d33746eb09e71e32f6
Dan Jaheny <danjaheny@mac.home>
2025-10-10 21:52:21 +0200

    adding single contact and batch

--- python/address-book/add-contact.py
+++ python/address-book/add-contact.py
@@ -12,35 +12,30 @@ load_dotenv()
 ## CONFIG
 USER_API_TOKEN = os.getenv("FORDEFI_API_TOKEN")
 PRIVATE_KEY_PEM_FILE = Path("./secret/private.pem")
-name = "Pendle router"
-chain_type = "evm"
-group_ids = ["82a840da-78ca-439d-b874-0fd7daf54fb4"]
-chains = ["evm_ethereum_mainnet"] # for CUSTOM evm chains use "evm_chainId", for example evm_747474
-contact_address = "0x888888888889758F76e7103c6CbF23ABbF58F946"
 path = "/api/v1/addressbook/contacts"
 
 async def main():
     try:
-        ## Building transaction payload
+        ## Construct payload
         request_json = {
-            "name": name,
-            "group_ids": group_ids,
-            "type": chain_type,
-            "address": contact_address,
-            "chains": chains
+            "name": "Pendle router",
+            "group_ids": ["82a840da-78ca-439d-b874-0fd7daf54fb4"], # Optional
+            "type": "evm",
+            "address": "0x888888888889758F76e7103c6CbF23ABbF58F946",
+            "chains": ["evm_ethereum_mainnet"] # for CUSTOM evm chains use "evm_chainId", for example evm_747474
         }
         request_body = json.dumps(request_json)
         timestamp = datetime.datetime.now().strftime("%s")
         payload = f"{path}|{timestamp}|{request_body}"
 
-        ## Signing transaction payload with API User's private key
+        ## Sign batch payload with API User's private key
         signature = await sign(payload=payload, private_key_path=PRIVATE_KEY_PEM_FILE)
 
-        ## Broadcasting signed payload to Fordefi
+        ## Send signed payload to Fordefi
         await broadcast_tx(path, USER_API_TOKEN, signature, timestamp, request_body)
-        print("✅ Transaction submitted successfully!")
+        print("✅ New contact submitted successfully!")
     except Exception as e:
-        print(f"❌ Transaction failed: {str(e)}")
+        print(f"❌ Adding contact failed: {str(e)}")
 
 if __name__ == "__main__":
     asyncio.run(main())
