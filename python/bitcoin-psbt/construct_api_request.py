3225385e136a7a0dfefb6bd523259747061974fc
Dan Jaheny <danjaheny@mac.home>
2025-11-28 12:32:07 +0100

    evm raw contract

--- python/bitcoin-psbt/construct_api_request.py
+++ python/bitcoin-psbt/construct_api_request.py
@@ -3,11 +3,11 @@ from typing import Any
 async def build_request(vault_id: str, vault_address: str, psbt_hex_data: str, will_auto_finalize: bool, is_bitcoin_mainnet: bool) -> dict[str, Any]:
     print(f'Preparing transaction from Vault {vault_id}')
     print(f"Vault address: {vault_address}")
-    if vault_address.startswith('bc1q'):
+    if vault_address.startswith(('bc1q', 'tb1q')):
         address_type = "segwit"
-    elif vault_address.startswith('bc1p'):
+    elif vault_address.startswith(('bc1p', 'tb1p')):
         address_type = "taproot"
-    elif vault_address.startswith('1') or vault_address.startswith('3'):
+    elif vault_address.startswith(('1', '3', 'm', 'n', '2')):
         raise ValueError(f"Legacy Bitcoin addresses are not supported. Address: {vault_address}")
     else:
         raise ValueError(f"Unknown Bitcoin address format: {vault_address}")
@@ -27,7 +27,7 @@ async def build_request(vault_id: str, vault_address: str, psbt_hex_data: str, w
                     "address_type": address_type,
                     "chain": {
                         "chain_type": "utxo",
-                        "unique_id": "bitcoin_mainnet" if is_bitcoin_mainnet else "bitcoin_testnet"
+                        "unique_id": "bitcoin_mainnet" if is_bitcoin_mainnet else "bitcoin_testnet" ## testnet v3
                     },
                 },
                 "inputs": [ # OPTIONAL array describing how each input will be signed
