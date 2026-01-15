d9065bf60a9de4d235dff49ba44954c906826c1a
Dan Jaheny <danjaheny@mac.home>
2026-01-15 16:11:02 +0100

    adding sponsorship examples

---
+++ python/simple-api-transfers/move-chains/aptos/sponsored_fungible_assets_aptos.py
@@ -0,0 +1,72 @@
+import os
+import json
+import asyncio
+import datetime
+from utils.broadcast import broadcast_tx
+from utils.sign_payload import sign
+from dotenv import load_dotenv
+
+load_dotenv()
+
+async def build_sponsored_tx(vault_id: str, destination: str, custom_note: str, value: str, fee_payer: str, token_contract: str):
+    request_json = {
+        "signer_type": "api_signer",
+        "type": "aptos_transaction",
+        "details": {
+            "fee_payer": {
+                "type": "vault",
+                "vault_id": fee_payer
+            },
+            "type": "aptos_transfer",
+            "to": {
+                "type": "hex",
+                "address": destination
+            },
+            "value": {
+                "type": "value",
+                "value": value
+            },
+            "asset_identifier": {
+                "type": "aptos",
+                "details": {
+                    "type": "new_coin",
+                    "new_coin_type":{
+                        "chain": "aptos_mainnet",
+                        "metadata_address": token_contract
+                    }
+                }
+            }
+        },
+        "note": custom_note,
+        "vault_id": vault_id
+    }
+    
+    return request_json
+
+## Fordefi configuration
+USER_API_TOKEN = os.environ["FORDEFI_API_TOKEN"]
+APTOS_VAULT_ID = os.environ["APTOS_VAULT_ID"]
+FEE_PAYER_VAULT_ID = os.environ["FEE_PAYER_VAULT_ID_APTOS"]
+path = "/api/v1/transactions"
+destination = "0x125cdce37fe906619ffe12c4d411041f4de297c4b7667042a6fe3f3e1c9edcc6" # CHANGE to your destination address
+token_contract = "0xbae207659db88bea0cbead6da0ed00aac12edcdda169e591cd41c94180b46f3b"
+custom_note = "hello sponsored Sui transations!" # Optional note
+value = str(10_000) # 1 USDC = 1_000_000
+
+async def main():
+    try:
+        ## Building transaction
+        request_json = await build_sponsored_tx(vault_id=APTOS_VAULT_ID, destination=destination, custom_note=custom_note, value=value, fee_payer=FEE_PAYER_VAULT_ID, token_contract=token_contract)
+        request_body = json.dumps(request_json)
+        timestamp = datetime.datetime.now().strftime("%s")
+        payload = f"{path}|{timestamp}|{request_body}"
+         ## Signing transaction with API User private key
+        signature = await sign(payload=payload)
+       ## Push tx to Fordefi for MPC signing and broadcast to network
+        await broadcast_tx(path, USER_API_TOKEN, signature, timestamp, request_body)
+        print("✅ Transaction submitted successfully!")
+    except Exception as e:
+        print(f"❌ Transaction failed: {str(e)}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
